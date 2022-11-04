71f185e2cf7571e55fe3e069b962de0d7d6f49fd
Martin Widarmadi <2072010@maranatha.ac.id>
2022-11-04 12:30:36 +0700

    Update index.html
    Add responsive and sent data to google forms

--- scripts/index.js
+++ scripts/index.js
@@ -13,3 +13,31 @@ const openTab = (tabName) => {
   event.currentTarget.classList.add('active-link');
   document.querySelector(`#${tabName}`).classList.add('active-tab');
 };
+
+const sideMenu = document.querySelector('#sideMenu');
+
+const openMenu = () => {
+  sideMenu.style.right = '0';
+  console.log('lalala');
+};
+
+const closeMenu = () => {
+  sideMenu.style.right = '-200px';
+};
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx6hPSLGdDE0Qg1-lIEUILUfd9tUDqZoqse5riD2trcHRcTnUdyQWQS8TCFb6MAdp0Reg/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.querySelector('#msg');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = 'Message sent successfully';
+      setTimeout(() => {
+        msg.innerHTML = '';
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error('Error!', error.message));
+});
